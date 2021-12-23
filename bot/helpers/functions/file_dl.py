cde2e954d19d3021923f2796cbf5a66ccbd5ad47
Vinayak Mundakkal <playstorepointsusa@gmail.com>
2021-12-23 17:21:12 +0530

    pyro seems harsh with me

--- bot/helpers/functions/file_dl.py
+++ bot/helpers/functions/file_dl.py
@@ -1,5 +1,4 @@
 import time
-import asyncio
 from pyaiodl import Downloader
 from bot import Config, LOGGER
 from bot.helpers.translations import lang
@@ -9,23 +8,19 @@ from pyrogram.errors import MessageNotModified
 start_time = time.time()
 dl = Downloader(download_path=Config.DOWNLOAD_LOCATION, chunk_size=Config.CHUNK_SIZE*1000)
 
-async def file_dl(bot, update, init_msg, link, s_vid, s_pht):
+async def file_dl(bot, update, init_msg, msg_id, link, s_vid, s_pht):
     uuid = await dl.download(link)
     while await dl.is_active(uuid):
         status = await dl.status(uuid)
         filename = status['filename']
         detail_msg = await progress_for_aiodl(status)
         try:
-            msg = await bot.send_message(
+            await bot.edit_message_text(
                 chat_id=update.chat.id,
-                text=lang.START_DOWNLOAD,
-                reply_to_message_id=init_msg.reply_to_message.message_id
+                text=detail_msg,
+                message_id=init_msg.message_id,
+                parse_mode="html"
             )
-            asyncio.sleep(3)
-            await msg.edit_text(
-                text=detail_msg
-            )
-            #await msg.edit(detail_msg)
         except MessageNotModified:
             pass
         except Exception as e:
@@ -39,10 +34,10 @@ async def file_dl(bot, update, init_msg, link, s_vid, s_pht):
                 caption=filename,
                 supports_streaming=s_vid,
                 progress=progress_for_pyrogram,
-                reply_to_message_id=init_msg.reply_to_message.message_id,
+                reply_to_message_id=msg_id,
                 progress_args=(
                     lang.INIT_UPLOAD_FILE,
-                    msg,
+                    init_msg,
                     start_time
                 )
             )
@@ -52,10 +47,10 @@ async def file_dl(bot, update, init_msg, link, s_vid, s_pht):
                 photo=Config.DOWNLOAD_LOCATION + "/" + filename,
                 caption=filename,
                 progress=progress_for_pyrogram,
-                reply_to_message_id=init_msg.reply_to_message.message_id,
+                reply_to_message_id=msg_id,
                 progress_args=(
                     lang.INIT_UPLOAD_FILE,
-                    msg,
+                    init_msg,
                     start_time
                 )
             )
@@ -65,10 +60,10 @@ async def file_dl(bot, update, init_msg, link, s_vid, s_pht):
                 document=Config.DOWNLOAD_LOCATION + "/" + filename,
                 caption=filename,
                 progress=progress_for_pyrogram,
-                reply_to_message_id=init_msg.reply_to_message.message_id,
+                reply_to_message_id=msg_id,
                 progress_args=(
                     lang.INIT_UPLOAD_FILE,
-                    msg,
+                    init_msg,
                     start_time
                 )
             )
