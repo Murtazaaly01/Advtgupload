40e59a3d8b5c31b7bf4fd21dcacc9ac0d4528590
Vinayak Mundakkal <playstorepointsusa@gmail.com>
2021-12-23 19:33:06 +0530

    f

--- bot/helpers/functions/gen_thumb.py
+++ bot/helpers/functions/gen_thumb.py
@@ -3,18 +3,15 @@ import ffmpeg
 from bot import LOGGER, Config
 
 def generate_thumbnail(file, msg_id):
-    thumb_name = Config.DOWNLOAD_LOCATION + f"{msg_id}-Thumbnail.jpg"
+    thumb_name = f"{Config.DOWNLOAD_LOCATION}/{msg_id}-Thumbnail.jpg"
     probe = ffmpeg.probe(file)
     time = float(probe['streams'][0]['duration']) // 2
     width = probe['streams'][0]['width']
-    try:
-        (
-            ffmpeg
-            .input(file, ss=time)
-            .filter('scale', width, -1)
-            .output(thumb_name, vframes=1)
-            .overwrite_output()
-            .run(capture_stdout=True, capture_stderr=True)
-        )
-    except ffmpeg.Error as e:
-        LOGGER.error(e.stderr.decode('utf-8'), file=sys.stderr)
+    (
+        ffmpeg
+        .input(file, ss=time)
+        .filter('scale', width, -1)
+        .output(thumb_name, vframes=1)
+        .overwrite_output()
+        .run(capture_stdout=True, capture_stderr=True)
+    )
