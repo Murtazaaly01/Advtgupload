eb1a572eeb6e3261882ac66d35fdce9545a335ab
Vinayak Mundakkal <playstorepointsusa@gmail.com>
2022-01-07 17:15:54 +0530

    forgot

--- bot/modules/tg_zip.py
+++
@@ -1,7 +0,0 @@
-from bot import Config, LOGGER, CMD
-from pyrogram import Client, filters
-from bot.helpers.translations import lang
-
-@Client.on_message(filters.command([CMD.UPLOAD, f"{CMD.UPLOAD}@{Config.BOT_USERNAME}"]))
-async def zip_tg_files(bot, update):
-    
