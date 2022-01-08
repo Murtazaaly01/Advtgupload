7226394913d45f8228e30ddb08df1ce555d19f27
Vinayak Mundakkal <76476634+vinayak-7-0-3@users.noreply.github.com>
2022-01-08 22:10:21 +0530

    Update file_dl.py

--- bot/helpers/functions/file_dl.py
+++ bot/helpers/functions/file_dl.py
@@ -35,7 +35,7 @@ async def file_dl(bot, update, link, init_msg, reply_to_id):
 
     if filename != "Unknown":
         s_vid, s_pht = await checkUserSet(update.from_user.id)
-        await pyro_upload(bot, update, file_path, filename, s_vid, s_pht, reply_to_id, init_msg)
+        await pyro_upload(bot, update, file_path, filename, bool(s_vid), bool(s_pht), reply_to_id, init_msg)
     else:
         await bot.send_message(
             chat_id=update.chat.id,
