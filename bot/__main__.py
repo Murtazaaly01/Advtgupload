c05d9e95ad588c49f7ebaa3007ee1f49ac9eee42
Vinayak Mundakkal <playstorepointsusa@gmail.com>
2022-01-09 15:03:14 +0530

    oophs

--- bot/__main__.py
+++ bot/__main__.py
@@ -11,8 +11,8 @@ LOGGER = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 if __name__ == "__main__":
-    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-        os.makedirs(Config.DOWNLOAD_LOCATION)
+    if not os.path.isdir(Config.DOWNLOAD_BASE_DIR):
+        os.makedirs(Config.DOWNLOAD_BASE_DIR)
     plugins = dict(
         root="bot/modules"
     )
