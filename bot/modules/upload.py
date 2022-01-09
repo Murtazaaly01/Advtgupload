2b923958d34d8bc64f25ad5b5f7ad50a8b1b773c
Vinayak Mundakkal <76476634+vinayak-7-0-3@users.noreply.github.com>
2022-01-09 09:14:04 +0530

    Link as reply

--- bot/modules/upload.py
+++ bot/modules/upload.py
@@ -8,15 +8,19 @@ from bot.helpers.database.database import check_user
 async def upload(bot, update):
     user_id = update.from_user.id
     await check_user(user_id)
-    reply_to_id = update.message_id
     try:
         link = update.text.split(" ", maxsplit=1)[1]
+        reply_to_id = update.message_id 
     except:
-        return await bot.send_message(
-            chat_id=update.chat.id,
-            text=lang.ERR_USAGE,
-            reply_to_message_id=update.message_id
-        )
+        try:
+            link = update.reply_to_message.text
+            reply_to_id = update.reply_to_message.message_id
+        except:
+            return await bot.send_message(
+                chat_id=update.chat.id,
+                text=lang.ERR_USAGE,
+                reply_to_message_id=update.message_id
+            )
     init_msg = await bot.send_message(
         chat_id=update.chat.id,
         text=lang.INIT_DOWNLOAD_FILE,
