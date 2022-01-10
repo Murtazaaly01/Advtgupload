1f48156f8d40a760bb08ac289a817b0b8a3ca77e
Vinayak Mundakkal <playstorepointsusa@gmail.com>
2022-01-10 21:37:51 +0530

    Update screenshots.py

--- bot/modules/screenshots.py
+++ bot/modules/screenshots.py
@@ -47,20 +47,25 @@ async def screenshots(bot, update):
             try:
                 link = update.text.split(" ", maxsplit=1)[1]
                 reply_to_id = update.message_id 
+                try:
+                    ss_no = link.split(" ", maxsplit=1)[1]
+                    link = link.split(" ", maxsplit=1)[0]
+                except:
+                    ss_no = Config.DEFAULT_SS_GEN_LIM
             except:
                 try:
                     link = update.reply_to_message.text
                     reply_to_id = update.reply_to_message.message_id
+                    try:
+                        ss_no = update.text.split(" ", maxsplit=1)[1]
+                    except:
+                        ss_no = Config.DEFAULT_SS_GEN_LIM
                 except:
                     return await bot.send_message(
                         chat_id=update.chat.id,
                         text=lang.ERR_USAGE,
                         reply_to_message_id=update.message_id
                     )
-            try:
-                ss_no = link.split(" ", maxsplit=1)[1]
-            except:
-                ss_no = Config.DEFAULT_SS_GEN_LIM
 
             init_msg = await bot.send_message(
                 chat_id=update.chat.id,
@@ -92,6 +97,7 @@ async def screenshots(bot, update):
                 os.makedirs(ss_dir)
             images = await generate_screenshot(file_path, ss_dir, int(ss_no))
             video, photo = await checkUserSet(update.from_user.id)
+            print(video, photo)
             for image in images:
                 await pyro_upload(bot, update, image, '', video, photo, reply_to_id, init_msg)
                 await asyncio.sleep(1)
